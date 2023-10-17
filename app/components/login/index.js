ae8e06c025e06226da824b2b94da0da43b5ae40b
Karen Irigoyen <krn21109718@gmail.com>
2023-10-16 21:16:21 -0700

    Login and register creation

--- app/components/login/index.js
+++ app/components/login/index.js
@@ -1,28 +1,43 @@
 "use client"
+import { Button, Stack, TextField, Typography } from "@mui/material";
 import { montserrat } from "../styles";
-
-const { Code } = require("@nextui-org/react");
+import Link from "next/link";
+import { useState } from "react";
+import Register from "../register/register";
 
 export default function LogIn() {
 	// LETS USE THIS TO ADD A LOGIN FORM
+	const [open, setOpen] = useState(false);
+	const handleClose = () => setOpen(false);
+
 	return (
-		<div style={{
-			display: "flex",
-			alignItems: "center",
-			justifyContent: "center",
-			height: "8vh",
-			width: "4vw",
-			padding: "0 100px",
-			border: "none",
+		<Stack sx={{
+			background: "#fff",
+			borderRadius: "15px",
+			padding: "30px 20px",
+			gap: "20px",
+			width: "400px"
 		}}>
-			<a href="/">
-				<h4 className={montserrat.className} style={{
-					fontSize: "20px",
-					fontWeight: "300",
-					color: "#333333",
-				}}>Login</h4>
-			</a>
-		</div>
+			{/* REGISTER MODAL */}
+			<Register open={open} handleClose={handleClose} />
+			<Typography variant="h4" component="h1">
+				Login
+			</Typography>
+			<TextField id="outlined-basic" label="Email" variant="outlined" type="email" />
+			<TextField id="outlined-basic" label="Password" variant="outlined" type="password" />
+			<Button variant="contained">Login</Button>
+			<Typography>
+				Do you have an account? {" "}
+				<Button variant="text"
+					onClick={()=>{
+						setOpen(true)
+					}}
+					sx={{
+						textTransform: "capitalize"
+					}}
+				>Register</Button>
+			</Typography>
+		</Stack>
 	)
 }
 
